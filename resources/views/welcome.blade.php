76cf0f9b83774a6968aae2f1f76bde9cded8db23
LeHongMinh-ST <minhhl298.st@gmail.com>
2019-12-04 20:22:28 +0700

    firt file

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <title>About me</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="About me">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,400i,600,600i,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="assets/css/style.css"/>
+
+
+</head>
+<body>
+
+<section class="hero-section spad">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xl-10 offset-xl-1">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="hero-text">
+                            <h2>Lê Hồng Minh</h2>
+                            <p>Mục tiêu sau khi hoàn thành xong khóa học là sau này đi làm kiếm được công việc 2000$</p>
+                        </div>
+                        <div class="hero-info">
+                            <h2>Thông tin</h2>
+                            <ul>
+                                <li><span>Ngày sinh</span>29 Tháng 8, 2000</li>
+                                <li><span>Trường </span>Học viện nông nghiệp Việt Nam</li>
+                                <li><span>E-mail</span>minhhl298.st@gmail.com</li>
+                                <li><span>Số điện thoại</span>0974798960</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <figure class="hero-image">
+                            <img src="assets/img/hero.jpg">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<script src="assets/js/jquery-2.1.4.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/main.js"></script>
+</body>
+</html>
