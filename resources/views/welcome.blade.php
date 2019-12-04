6e020c3bf2f03a29263f6b68703f02bfdca78466
LeHongMinh-ST <minhhl298.st@gmail.com>
2019-12-04 23:19:57 +0700

    firt commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,8 +46,5 @@
     </div>
 </section>
 
-<script src="assets/js/jquery-2.1.4.min.js"></script>
-<script src="assets/js/bootstrap.min.js"></script>
-<script src="assets/js/main.js"></script>
 </body>
 </html>
