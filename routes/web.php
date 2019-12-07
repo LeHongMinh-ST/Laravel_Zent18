798ac0f5389ad9c3b0f7f4963339addd11cdd834
LeHongMinh-ST <minhhl298.st@gmail.com>
2019-12-07 10:02:06 +0700

    Tạo route và group cho tính năng hoàn thành và làm lại

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,48 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('user/{id?}', function ($id = Null) {
+    if ($id == NULL) {
+        return "all";
+    }
+    return "user " . $id;
+});
+
+Route::get('user/{id}/post/{idPost}/{userName}', function ($id, $idPost, $userName) {
+    echo "This is post " . $idPost . " of user " . $id . " username: " . $userName;
+});
+
+Route::get('/thanhcong/{id}', function ($id) {
+    dd("Thành công " . $id);
+//   return redirect('/');
+});
+
+Route::delete('task/delete/{id}', function ($id) {
+    return redirect('/thanhcong/' . $id);
+})->name('todo.task.delete');
+
+//Route::prefix('task')->group(function (){
+//    Route::delete('delete/{id}', function ($id) {
+//        return redirect('/thanhcong/' . $id);
+//    })->name('todo.task.delete');
+//});
+
+Route::group([
+    'prefix' => 'task'
+], function () {
+    Route::delete('delete/{id}', function ($id) {
+        return redirect('/thanhcong/' . $id);
+    })->name('todo.task.delete');
+
+    Route::get('complete/3', function () {
+        dd("Hoàn Thành");
+    })->name('todo.task.complete');
+
+    Route::get('reset/3', function () {
+        dd("Làm lại");
+    })->name('todo.task.reset');
+});
+
